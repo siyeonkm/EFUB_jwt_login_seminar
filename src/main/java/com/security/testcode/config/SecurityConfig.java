a6c8a39582a77875124fc583fee109d8a785c98d
siyeonkm <68330823+siyeonkm@users.noreply.github.com>
2021-10-01 16:38:11 +0900

    Fix: "form 로그인 disable하기"

--- src/main/java/com/security/testcode/config/SecurityConfig.java
+++ src/main/java/com/security/testcode/config/SecurityConfig.java
@@ -24,6 +24,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
+                .httpBasic().disable()
+                .formLogin().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
